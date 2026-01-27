bd6276852a6fe67e86d9c9fc7ffbe7095e6677a3
TECHSAVVY2023 <info@techsavvies.space>
2026-01-27 19:53:00 +0800

    update

--- pages/cms/dashboard/index.vue
+++ pages/cms/dashboard/index.vue
@@ -287,6 +287,7 @@ const contentWritersEmails = [
   "xie.medrano@lsu.edu.ph",
   "jenny.licanda@lsu.edu.ph",
   "jorenlee.luna@lsu.edu.ph",
+  "edm@lsu.edu.ph"
 ];
 const hrMenuEmails = ["hr@lsu.edu.ph", "npc@lsu.edu.ph"];
 const libraryMenuEmails = [
