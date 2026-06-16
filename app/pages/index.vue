50a88979ed7520e96c3a504c4a96ee83925bf9be
jorenlee <jorenleeluna24@gmail.com>
2026-06-16 16:55:34 +0800

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -33,12 +33,16 @@ onBeforeUnmount(() => {
     <Header />
     <div class="relative">
       <div class="bg-[#e3e3e3] relative">
-        <img
+        <!-- <img
           src="https://lsu-media-styles.sgp1.digitaloceanspaces.com/Logos/Corporate%20Logo%20New/Corporate%20Logo-Tagline.png"
           class="absolute left-1/2 -translate-x-1/2 lg:top-24 top-16 lg:w-4/12 w-6/12"
         />
         <img
           src="https://lsu-media-styles.sgp1.digitaloceanspaces.com/LANDSCAPE%20CROPPED.jpg"
+          class="lg:pt-3 pt-14 mx-auto w-full"
+        /> -->
+        <img
+          src="https://lsu-media-styles.sgp1.digitaloceanspaces.com/lsu-media-styles/cms/data/uploads/Graduation2026-125.png"
           class="lg:pt-3 pt-14 mx-auto w-full"
         />
       </div>
