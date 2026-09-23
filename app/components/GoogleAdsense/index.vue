0625921af7b8cf33fd7202077e21a18383f762f8
jorenlee <jorenleeluna24@gmail.com>
2026-09-23 09:42:40 +0800

    update

--- app/components/GoogleAdsense/index.vue
+++ app/components/GoogleAdsense/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="isClient">
+  <div v-if="isClient" class="flex justify-center">
     <!-- Use the reusable Adsbygoogle component which waits for the AdSense script to load -->
     <Adsbygoogle ad-slot="1745428489" class="mb-5"/>
      <Adsbygoogle ad-slot="2306527650" />
